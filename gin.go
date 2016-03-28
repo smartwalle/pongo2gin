14f026d64bdfcfb1028e25f361dbd82542867443
Yangfeng <smartwalle@gmail.com>
2016-03-28 18:57:15 +0800

    init

---
+++ gin.go
@@ -0,0 +1,43 @@
+package pongo2gin
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin/render"
+	"github.com/smartwalle/pongo2render"
+)
+
+//	var router = gin.Default()
+//	router.HTMLRender = pongo2render.NewGinRender("./templates")
+//
+//	router.GET("/m", func(c *gin.Context) {
+//		c.HTML(200, "index.html", pongo2.Context{"aa": "eee"})
+//	})
+//	router.Run("localhost:9005")
+
+type GinRender struct {
+	pongo2render.Render
+}
+
+type GinHTML struct {
+	*pongo2render.HTML
+	data interface{}
+}
+
+func NewGinRender(templateDir string) *GinRender {
+	var r = &GinRender{}
+	r.TemplateDir = templateDir
+	return r
+}
+
+func (this GinRender) Instance(name string, data interface{}) render.Render {
+	var gHtml = &GinHTML{}
+	var h = this.GetHTML(name)
+	gHtml.HTML = h
+	gHtml.data = data
+	return gHtml
+}
+
+func (this *GinHTML) Render(w http.ResponseWriter) (err error) {
+	return this.HTML.ExecuteWriter(w, this.data)
+}
+
