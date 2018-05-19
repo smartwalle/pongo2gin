15f461630e357013a9426d952ddce61c2645ded7
SmartWalle <smartwalle@gmail.com>
2018-05-19 08:55:22 +0800

    Update

--- gin.go
+++ gin.go
@@ -7,45 +7,55 @@ import (
 )
 
 //	var router = gin.Default()
-//	router.HTMLRender = pongo2render.NewGinRender("./templates")
+//	router.HTMLRender = pongo2gin.NewHTMLRender("./templates")
 //
 //	router.GET("/m", func(c *gin.Context) {
-//		c.HTML(200, "index.html", pongo2.Context{"aa": "eee"})
+//		c.HTML(200, "index.html", pongo2.Context{"key": "value"})
 //	})
 //	router.Run("localhost:9005")
 
-type GinRender struct {
+// --------------------------------------------------------------------------------
+type HTMLRender struct {
 	pongo2render.Render
 }
 
-type GinTemplate struct {
-	*pongo2render.Template
-	data interface{}
-}
-
-func NewGinRender(templateDir string) *GinRender {
-	var r = &GinRender{}
+func NewHTMLRender(templateDir string) *HTMLRender {
+	var r = &HTMLRender{}
 	r.TemplateDir = templateDir
 	return r
 }
 
-func (this GinRender) Instance(name string, data interface{}) render.Render {
-	var gHtml = &GinTemplate{}
+func (this *HTMLRender) Instance(name string, data interface{}) render.Render {
+	var gHtml = &Template{}
 	var h = this.Template(name)
 	gHtml.Template = h
 	gHtml.data = data
 	return gHtml
 }
 
-func (this *GinTemplate) Render(w http.ResponseWriter) (err error) {
+func (this *HTMLRender) InstanceFromString(tpl string, data interface{}) render.Render {
+	var gHtml = &Template{}
+	var h = this.TemplateFromString(tpl)
+	gHtml.Template = h
+	gHtml.data = data
+	return gHtml
+}
+
+// --------------------------------------------------------------------------------
+type Template struct {
+	*pongo2render.Template
+	data interface{}
+}
+
+func (this *Template) Render(w http.ResponseWriter) (err error) {
 	return this.Template.ExecuteWriter(w, this.data)
 }
 
-func (this *GinTemplate) WriteContentType(w http.ResponseWriter) {
+func (this *Template) WriteContentType(w http.ResponseWriter) {
 	pongo2render.WriteContentType(w, []string{"text/html; charset=utf-8"})
 }
 
-////////////////////////////////////////////////////////////////////////////////
+// --------------------------------------------------------------------------------
 const k_PONGO_TO_GIN_KEY = "pongo2gin"
 
 type Context interface {
@@ -54,10 +64,10 @@ type Context interface {
 	MustGet(key string) interface{}
 }
 
-func FromContext(ctx Context) *GinRender {
-	return ctx.MustGet(k_PONGO_TO_GIN_KEY).(*GinRender)
+func FromContext(ctx Context) *HTMLRender {
+	return ctx.MustGet(k_PONGO_TO_GIN_KEY).(*HTMLRender)
 }
 
-func ToContext(ctx Context, r *GinRender) {
+func ToContext(ctx Context, r *HTMLRender) {
 	ctx.Set(k_PONGO_TO_GIN_KEY, r)
 }
